92a5e5716679d9b13881927c05709e7b2cecb221
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2026-01-12 13:56:29 +0100

    implement card and style done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { Container, Grid } from '@mui/material';
 import Footer from "./footer/footer";
 import HeaderBar from './header/HeaderBar'
-import Test from "./Test";
+import MarketCard from './components/MarketCard/MarketCard'
+import Data from '../data.json'
 import './App.css'
 
 export default function App() {
   return (
     <div>
       <HeaderBar />
-       <Test/>
-       <Footer />
+      
+      <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3}>
+          {Data.markets.map((market) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={market.id}>
+              <MarketCard title={market.title} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      
+      <Footer />
     </div>
   );
 }
