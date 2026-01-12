5f3e5e7023535f5b10ce5502c79ed5d2c3875d4d
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-01-12 12:48:10 +0100

    Création du footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { Button, Container, Typography } from "@mui/material";
+import { Button, Container, Typography, Box } from "@mui/material";
+import Footer from "./footer/footer"; // importe ton footer
 
 export default function App() {
   return (
-    <Container sx={{ mt: 4 }}>
-      <Typography variant="h4" gutterBottom>
-        Hello MUI 👋
-      </Typography>
+    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      {/* Contenu principal centré */}
+      <Container sx={{ mt: 4, flex: 1, textAlign: "center" }}>
+        <Typography variant="h4" gutterBottom>
+          Hello MUI 👋
+        </Typography>
 
-      <Button variant="contained" color="primary">
-        Bouton MUI
-      </Button>
-    </Container>
+        <Button variant="contained" color="primary">
+          Bouton MUI
+        </Button>
+      </Container>
+
+      {/* Footer full width */}
+      <Footer />
+    </Box>
   );
 }
