9c9b24180345cd973eaf7f588f968263f47ba022
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-13 11:28:56 +0100

    add background colors

--- src/theme.js
+++ src/theme.js
@@ -13,13 +13,25 @@ export const theme = createTheme({
       secondary: "#899cb2",
     },
     success: { 
-      main: "#43c773",
+      main: "#325455",
     },
     error: {    
       main: "#e13737",
     },
     background: {
       default: "#1d2b3a",
+    },
+        marketCard: {          // cor personalizada para MarketCard
+      background: "#2F3F50",
+      yes:{
+        main:"#43c773",
+        background:'#325455'
+      } ,     // botão “Yes”
+      no:{ 
+        main:"#e13737",
+        background:"#4b3f4c",
+      },       // botão “No”
+      text: "#f2f2f2",     // texto dentro do card
     },
   },
 });
