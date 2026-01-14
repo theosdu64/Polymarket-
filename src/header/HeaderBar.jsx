44b7bb3db379107058d5833b3822dee71ddf116d
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2026-01-14 16:24:35 +0100

    refactor css header

--- src/header/HeaderBar.jsx
+++ src/header/HeaderBar.jsx
@@ -1,17 +1,12 @@
 import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import Logo from '../Logo'
 import LoginSignUpBtn from "../LoginSignUpBtn/LoginSignUpBtn";
 import HowItWorksButton from "../components/HowItWorkButton/HowItWorkButton";
 import BurgerMenu from "../components/BurgerMenu/BurgerMenu.jsx";
-import './header.css'
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative', 
@@ -20,13 +15,11 @@ const Search = styled('div')(({ theme }) => ({
   "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
-  marginLeft: 0,
-  width: "100%",
-  height: 48,
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(2),
-    width: "auto",
-  },
+  marginLeft: theme.spacing(3),
+  marginRight: theme.spacing(3),
+  height: 40,
+  flex: 1, 
+  maxWidth: 600, 
 }));
 
 const SearchIconWrapper = styled("div")(({ theme }) => ({
@@ -42,41 +35,34 @@ const SearchIconWrapper = styled("div")(({ theme }) => ({
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: 'inherit',
   width: '100%',
+  height: '100%',
   '& .MuiInputBase-input': {
     padding: theme.spacing(1, 1, 1, 0),
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
+    width: '100%',
   },
 }));
 
 export default function HeaderBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Logo />
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-          <div className='right-container' >
-          <HowItWorksButton />
-          </div>
-          <LoginSignUpBtn />
-          <BurgerMenu />
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <Toolbar disableGutters sx={{ display: 'flex', alignItems: 'center', py: 2 }}>
+      <Logo />
+      
+      <Search>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="Search polymarket"
+          inputProps={{ 'aria-label': 'search' }}
+        />
+      </Search>
+      
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 10 }}>
+        <HowItWorksButton />
+        <LoginSignUpBtn />
+        <BurgerMenu />
+      </Box>
+    </Toolbar>
   );
 }
